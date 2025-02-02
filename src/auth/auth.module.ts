0f43647f57fe2eb91f11bacfa7213f2a3b79b4b2
alfan.mgindotech <alfan.mgindotech@gmail.com>
2025-02-02 22:07:39 +0700

    feat: adjust-auth

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -6,6 +6,7 @@ import { UserSchema } from './schemas/user.schema';
 import { PassportModule } from '@nestjs/passport';
 import { JwtModule } from '@nestjs/jwt';
 import { ConfigService } from '@nestjs/config';
+import { JwtStrategy } from './jwt.strategy';
 
 @Module({
   imports: [
@@ -25,7 +26,8 @@ import { ConfigService } from '@nestjs/config';
     MongooseModule.forFeature([{name: 'User', schema: UserSchema}])
   ],
 
-  providers: [AuthService],
-  controllers: [AuthController]
+  controllers: [AuthController],
+  providers: [AuthService, JwtStrategy],
+  exports: [JwtStrategy, PassportModule]
 })
 export class AuthModule {}
