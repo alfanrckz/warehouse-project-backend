af73a822d5ff86c8432b5803277ad1a8e84343bf
alfan.mgindotech <alfan.mgindotech@gmail.com>
2025-02-02 21:19:47 +0700

    feat: configuration

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,28 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { ProductBrandModule } from './product-brand/product-brand.module';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { MongooseModule } from '@nestjs/mongoose';
+import { AuthModule } from './auth/auth.module';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      envFilePath: '.env',
+      isGlobal: true
+    }),
+    MongooseModule.forRootAsync({
+      imports: [ConfigModule],
+      useFactory: async (configService: ConfigService) => ({
+        uri: configService.get<string>('DB_URI'),
+      }),
+      inject: [ConfigService],
+    }),
+    ProductBrandModule,
+    AuthModule
+    
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
